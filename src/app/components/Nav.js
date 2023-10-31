9a4c1cbfcfd53907140dbadb6bb5dd8693b4e816
Louisit0 <lrangarita7@gmail.com>
2023-10-30 22:28:50 -0300

    some routing

---
+++ src/app/components/Nav.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <ul className="flex gap-2">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
