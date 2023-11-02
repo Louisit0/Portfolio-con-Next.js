4b98ab740432e26e77fcad7b7042705b69d0000d
Louisit0 <lrangarita7@gmail.com>
2023-11-02 01:25:58 -0300

    new nav

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -3,19 +3,24 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <div>
-      <ul className="flex gap-2">
-        <li>
-          <Link href="/">Home</Link>
+    <nav className="flex flex-row justify-between py-10 text-slate-200">
+      <h1 className="text-3xl font-bold">Luis</h1>
+      <ul className="flex border border-white rounded-3xl px-4 py-2">
+        <li className="hover:bg-gray-800 rounded-3xl px-4">
+          <Link href="/">All</Link>
         </li>
-        <li>
+        <li className="hover:text-gray-500 rounded-3xl px-4 ">
           <Link href="/about">About</Link>
         </li>
-        <li>
+        <li className="hover:text-gray-500 rounded-3xl px-4 ">
+          <Link href="/contact">Projects</Link>
+        </li>
+        <li className="hover:text-gray-500 rounded-3xl px-4 ">
           <Link href="/contact">Contact</Link>
         </li>
       </ul>
-    </div>
+      <p>Resumé</p>
+    </nav>
   );
 };
 
