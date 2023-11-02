370b447fa143a5a1491f3a199bb0dd92459b5453
Luis <lrangarita7@gmail.com>
2023-11-02 18:05:16 -0300

    marcador por default

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className="bg-black leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
+        <div className="bg-black leading-relaxed text-slate-400 antialiased">
           <div className="mx-auto min-h-screen max-w-screen-xl py-12 font-sans  md:py-20 lg:py-0 lg:px-8">
             <div className={inter.className}>{children}</div>
           </div>
