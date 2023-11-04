ea8afbf775d3d3fa942a6ffdc8b397fdfd5e59f4
Louisit0 <lrangarita7@gmail.com>
2023-11-04 01:15:37 -0300

    componentes stack y about

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -1,33 +1,21 @@
-import React from "react";
+import Image from "next/image";
+import avatar from "../../assets/avatarluigi.jpeg";
 
 const About = () => {
   return (
-    <section
-      id="about"
-      className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
-      aria-label="About me"
-    >
-      <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
-        <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
-          About
-        </h2>
-      </div>
-      <div>
-        <p className="mb-4">
-          As a Junior Front-End Developer, I possess an impressive arsenal of
-          skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS. I excel in
-          designing and maintaining responsive websites that offer a smooth user
-          experience.
-        </p>
-        <p className="mb-4">
-          My expertise lies in crafting dynamic, engaging interfaces through
-          writing clean and optimized code and utilizing cutting-edge
-          development tools and techniques. I am also a team player who thrives
-          in collaborating with cross-functional teams to produce outstanding
-          web applications.
-        </p>
-      </div>
-    </section>
+    <div className="flex flex-col justify-center gap-4 bg-zinc-800 border-white rounded-3xl p-10 h-72">
+      <Image
+        className="rounded-full"
+        src={avatar}
+        width={100}
+        height={100}
+      ></Image>
+      <p>
+        Hi, i'm Luis a developer and product designer from Ireland. I'm
+        interested in React, Node, Product Design, Jamstack, Startups,
+        Cryptocurrencies and Music.
+      </p>
+    </div>
   );
 };
 
