1bf5fa82cf64606a193c7de1373f029fb692f0b0
Louisit0 <lrangarita7@gmail.com>
2023-11-03 23:42:35 -0300

    grid de skills

--- src/app/page.js
+++ src/app/page.js
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <>
       <Nav />
-      <div className="grid grid-cols-3 w-full gap-4 text-slate-200">
-        <div className="flex flex-col justify-center gap-6 bg-zinc-800 border-white rounded-3xl p-10 h-72">
+      <div className="grid grid-cols-3 w-full gap-4 text-slate-200 pb-10">
+        <div className="flex flex-col justify-center gap-4 bg-zinc-800 border-white rounded-3xl p-10 h-72">
           <Image
             className="rounded-full"
             src={avatar}
@@ -22,7 +22,7 @@ export default function Home() {
             Cryptocurrencies and Music.
           </p>
         </div>
-        <div className="flex flex-col justify-center gap-6 bg-zinc-800 border-white rounded-3xl p-10 h-72">
+        <div className="flex flex-col justify-center gap-4 bg-zinc-800 border-white rounded-3xl p-10 h-72">
           <Image
             className="rounded-full"
             src={avatar}
@@ -37,7 +37,7 @@ export default function Home() {
         </div>
         <div>
           <Image
-            className="flex flex-col justify-center gap-6 bg-zinc-800 border-white rounded-3xl h-72 object-cover"
+            className="flex flex-col justify-center gap-4 bg-zinc-800 border-white rounded-3xl h-72 object-cover"
             src={turisme}
             width={"100%"}
             height={"100%"}
@@ -45,21 +45,99 @@ export default function Home() {
         </div>
         <div>
           <Image
-            className="flex flex-col justify-center gap-6 bg-zinc-800 border-white rounded-3xl h-72 object-cover"
+            className="flex flex-col justify-center gap-4 bg-zinc-800 border-white rounded-3xl h-72 object-cover"
             src={pokedex}
             width={"100%"}
             height={"100%"}
           ></Image>
         </div>
-        <div className="grid grid-rows-2 h-72 gap-4">
-          <div className="flex flex-col justify-center gap-6 bg-zinc-800 border-white rounded-3xl p-10">
-            <h2>About me</h2>
+        <div className="grid grid-cols-4 gap-4">
+          <div className="flex flex-col justify-center bg-zinc-800 border-white rounded-3xl">
+            <p align="center">
+              <a href="https://skillicons.dev">
+                <img src="https://skillicons.dev/icons?i=html" />
+              </a>
+            </p>
           </div>
-          <div className="flex flex-col justify-center gap-6 bg-zinc-800 border-white rounded-3xl p-10">
-            <h2>ENJOY</h2>
+          <div className="flex flex-col justify-center bg-zinc-800 border-white rounded-3xl">
+            <p align="center">
+              <a href="https://skillicons.dev">
+                <img src="https://skillicons.dev/icons?i=css" />
+              </a>
+            </p>
+          </div>
+          <div className="flex flex-col justify-center bg-zinc-800 border-white rounded-3xl">
+            <p align="center">
+              <a href="https://skillicons.dev">
+                <img src="https://skillicons.dev/icons?i=javascript" />
+              </a>
+            </p>
+          </div>
+          <div className="flex flex-col justify-center bg-zinc-800 border-white rounded-3xl">
+            <p align="center">
+              <a href="https://skillicons.dev">
+                <img src="https://skillicons.dev/icons?i=typescript" />
+              </a>
+            </p>
+          </div>
+          <div className="flex flex-col justify-center bg-zinc-800 border-white rounded-3xl">
+            <p align="center">
+              <a href="https://skillicons.dev">
+                <img src="https://skillicons.dev/icons?i=tailwind" />
+              </a>
+            </p>
+          </div>
+          <div className="flex flex-col justify-center bg-zinc-800 border-white rounded-3xl">
+            <p align="center">
+              <a href="https://skillicons.dev">
+                <img src="https://skillicons.dev/icons?i=bootstrap" />
+              </a>
+            </p>
+          </div>
+          <div className="flex flex-col justify-center bg-zinc-800 border-white rounded-3xl">
+            <p align="center">
+              <a href="https://skillicons.dev">
+                <img src="https://skillicons.dev/icons?i=react" />
+              </a>
+            </p>
+          </div>
+          <div className="flex flex-col justify-center bg-zinc-800 border-white rounded-3xl">
+            <p align="center">
+              <a href="https://skillicons.dev">
+                <img src="https://skillicons.dev/icons?i=next" />
+              </a>
+            </p>
+          </div>
+          <div className="flex flex-col justify-center bg-zinc-800 border-white rounded-3xl">
+            <p align="center">
+              <a href="https://skillicons.dev">
+                <img src="https://skillicons.dev/icons?i=github" />
+              </a>
+            </p>
+          </div>
+          <div className="flex flex-col justify-center bg-zinc-800 border-white rounded-3xl">
+            <p align="center">
+              <a href="https://skillicons.dev">
+                <img src="https://skillicons.dev/icons?i=gitlab" />
+              </a>
+            </p>
+          </div>
+          <div className="flex flex-col justify-center bg-zinc-800 border-white rounded-3xl">
+            <p align="center">
+              <a href="https://skillicons.dev">
+                <img src="https://skillicons.dev/icons?i=mongo" />
+              </a>
+            </p>
+          </div>
+          <div className="flex flex-col justify-center bg-zinc-800 border-white rounded-3xl">
+            <p align="center">
+              <a href="https://skillicons.dev">
+                <img src="https://skillicons.dev/icons?i=net" />
+              </a>
+            </p>
           </div>
         </div>
-        <div className="flex flex-col justify-center gap-6 bg-zinc-800 border-white rounded-3xl p-10 h-72">
+        <div className="flex flex-col justify-center gap-4 bg-zinc-800 border-white rounded-3xl p-10 h-72">
           <Image
             className="rounded-full"
             src={avatar}
@@ -72,19 +150,44 @@ export default function Home() {
             Cryptocurrencies and Music.
           </p>
         </div>
-        <div className="grid grid-cols-4 h-20 gap-4">
-          <div className="flex flex-col justify-center gap-6 bg-zinc-800 border-white rounded-3xl p-10">
-            1
-          </div>
-          <div className="flex flex-col justify-center gap-6 bg-zinc-800 border-white rounded-3xl p-10">
-            1
-          </div>
-          <div className="flex flex-col justify-center gap-6 bg-zinc-800 border-white rounded-3xl p-10">
-            1
-          </div>
-          <div className="flex flex-col justify-center gap-6 bg-zinc-800 border-white rounded-3xl p-10">
-            1
-          </div>
+        <div className="flex flex-col justify-center gap-4 bg-zinc-800 border-white rounded-3xl p-10 h-72">
+          <Image
+            className="rounded-full"
+            src={avatar}
+            width={100}
+            height={100}
+          ></Image>
+          <p>
+            Hi, i'm Luis a developer and product designer from Ireland. I'm
+            interested in React, Node, Product Design, Jamstack, Startups,
+            Cryptocurrencies and Music.
+          </p>
+        </div>
+        <div className="flex flex-col justify-center gap-4 bg-zinc-800 border-white rounded-3xl p-10 h-72">
+          <Image
+            className="rounded-full"
+            src={avatar}
+            width={100}
+            height={100}
+          ></Image>
+          <p>
+            Hi, i'm Luis a developer and product designer from Ireland. I'm
+            interested in React, Node, Product Design, Jamstack, Startups,
+            Cryptocurrencies and Music.
+          </p>
+        </div>
+        <div className="flex flex-col justify-center gap-4 bg-zinc-800 border-white rounded-3xl p-10 h-72">
+          <Image
+            className="rounded-full"
+            src={avatar}
+            width={100}
+            height={100}
+          ></Image>
+          <p>
+            Hi, i'm Luis a developer and product designer from Ireland. I'm
+            interested in React, Node, Product Design, Jamstack, Startups,
+            Cryptocurrencies and Music.
+          </p>
         </div>
       </div>
     </>
