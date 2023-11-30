b061e99f40cffa718a2675da5308645b357d6f8d
Luis <lrangarita7@gmail.com>
2023-11-29 23:50:48 -0300

    Refactor About component and update soft skills section

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -2,53 +2,55 @@ import Image from "next/image";
 import { setup, ssn } from "../assets/Images";
 const About = () => {
   return (
-    <section className="flex flex-col max-w-screen-lg mx-auto px-8 md:px-20 lg:px-0 py-40">
+    <section className="flex flex-col max-w-screen-lg mx-auto px-8 md:px-20 lg:px-0 mt-10 md:mt-0">
       <section
         id="about"
-        className="flex flex-col lg:flex-row justify-evenly h-full gap-10 lg:gap-20 mt-20 lg:my-30 max-w-screen-lg mx-auto lg:px-0"
+        className="flex flex-col justify-evenly h-full gap-10 lg:gap-20 lg:my-30 max-w-screen-lg mx-auto lg:px-0"
       >
-        <ul className=" lg:w-full lg:mx-0">
-          <h3 className="text-4xl text-stone-800 font-bold tracking-tight mb-8">
-            About me
-          </h3>
-          <p className="text-zinc-600 text-base">
-            As a Junior Front-End Developer, I possess an impressive arsenal of
-            skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS. I excel
-            in designing and maintaining responsive websites that offer a smooth
-            user experience. My expertise lies in crafting dynamic, engaging
-            interfaces through writing clean and optimized code and utilizing
-            cutting-edge development tools and techniques. I am also a team
-            player who thrives in collaborating with cross-functional teams to
-            produce outstanding web applications.
-          </p>
+        <ul className="lg:flex lg:flex-row lg:w-full lg:gap-12 lg:mx-0">
+          <div className="flex flex-col">
+            <h3 className="text-4xl text-stone-800 font-bold tracking-tight mb-8">
+              About me
+            </h3>
+            <p className="text-zinc-600 text-base">
+              As a Junior Front-End Developer, I possess an impressive arsenal
+              of skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS. I
+              excel in designing and maintaining responsive websites that offer
+              a smooth user experience. My expertise lies in crafting dynamic,
+              engaging interfaces through writing clean and optimized code and
+              utilizing cutting-edge development tools and techniques. I am also
+              a team player who thrives in collaborating with cross-functional
+              teams to produce outstanding web applications.
+            </p>
+            <div className="my-8 lg:mb-0">
+              <h4 className="text-2xl text-stone-800 font-semibold tracking-tight mb-4">
+                Soft skills
+              </h4>
+              <ul className="flex flex-wrap gap-2 text-sm lg:justify-start">
+                <li className="px-3 py-1 border border-zinc-300 font-medium text-black rounded-full">
+                  Meticulousness 👁️
+                </li>
+                <li className="px-3 py-1 border border-zinc-300 font-medium text-black rounded-full">
+                  Adaptability 🔄
+                </li>
+                <li className="px-3 py-1 border border-zinc-300 font-medium text-black rounded-full">
+                  Problem Solving 🧩
+                </li>
+                <li className="px-3 py-1 border border-zinc-300 font-medium text-black rounded-full">
+                  Proactive 🚀
+                </li>
+              </ul>
+            </div>
+          </div>
+          <Image
+            className="flex flex-col w-full h-96 lg:h-80 justify-center object-cover rounded-2xl md:mx-auto my-auto lg:mx-0"
+            src={setup}
+            alt="setup"
+          ></Image>
         </ul>
-        <div>
-          <h4 className="text-2xl text-stone-800 font-semibold tracking-tight mb-4">
-            Soft skills
-          </h4>
-          <ul className="flex flex-wrap gap-2 text-sm lg:justify-start">
-            <li className="px-3 py-1 border border-zinc-300 font-medium text-black rounded-full">
-              Meticulousness 👁️
-            </li>
-            <li className="px-3 py-1 border border-zinc-300 font-medium text-black rounded-full">
-              Adaptability 🔄
-            </li>
-            <li className="px-3 py-1 border border-zinc-300 font-medium text-black rounded-full">
-              Problem Solving 🧩
-            </li>
-            <li className="px-3 py-1 border border-zinc-300 font-medium text-black rounded-full">
-              Proactive 🚀
-            </li>
-          </ul>
-        </div>
-        <Image
-          className="flex flex-col w-full h-96 lg:h-80 justify-center object-cover rounded-2xl md:mx-auto mb-40 lg:mx-0"
-          src={setup}
-          alt="setup"
-        ></Image>
       </section>
 
-      <div>
+      <div className="my-40">
         <h3 className="text-4xl text-stone-800 font-bold tracking-tight mb-8">
           Work experience
         </h3>
